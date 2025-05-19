5e6c0e7ba8a7e78c0a4a1df7ea6f9a218ad5a39a
João Vitor <jvitordiass@outlook.com>
2025-05-19 15:32:45 -0300

    ✨ feat: add docker compose and configure mysql with typeorm
    - create docker compose file for mysql and nest-template services
    - add mysql2 and typeorm dependencies in package.json
    - configure TypeOrmModule in app.module.ts for MySQL connection

--- 02-primeiros-passos-com-docker/src/app.module.ts
+++ 02-primeiros-passos-com-docker/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'local_user',
+      password: 'local_password',
+      database: 'nest-template-db',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
